abc99bdf637a8d02d7e7a6d2d852ec7e49f815e2
ektasingh1234 <148713572+ektasingh1234@users.noreply.github.com>
2024-11-25 21:49:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Placeholder for interactivity (animation, interactions, etc.)
+document.getElementById('getStarted').addEventListener('click', () => {
+  alert('Redirecting to the sign-up page!');
+});
+
+document.getElementById('learnMore').addEventListener('click', () => {
+  alert('Redirecting to more information!');
+});
